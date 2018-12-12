ab075a82088884d60b1ab88bed5fe81e95e0a996
Maxim <therzmaxim@gmail.com>
2018-12-12 18:13:42 +0300

    addition of requirements

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-updater = Updater(token='700348901:AAGMYDZZmafglP6O9nAwn_yqpEfpzA8m_iw') # Токен API к Telegram
+updater = Updater(token='700348901:AAGMYDZZmafglP6O9nAwn_yqpEfpzA8m_iw')
 dispatcher = updater.dispatcher
 
 import logging
@@ -12,13 +12,9 @@ def textMessage(bot, update):
     response = 'Получил Ваше сообщение: ' + update.message.text
     bot.send_message(chat_id=update.message.chat_id, text=response)
 
-# Хендлеры
 start_handler = CommandHandler('start', start)
 text_message_handler = MessageHandler(Filters.text, textMessage)
-# Добавляем хендлеры в диспетчер
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(text_message_handler)
-# Начинаем поиск обновлений
 updater.start_polling(clean=True)
-# Останавливаем бота, если были нажаты Ctrl + C
 updater.idle()
