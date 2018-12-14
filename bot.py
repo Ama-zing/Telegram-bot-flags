90e0769f92d7921e4dcd2af5776a8e9b3291fe15
Maxim <therzmaxim@gmail.com>
2018-12-14 22:23:33 +0300

    photo

--- bot.py
+++ bot.py
@@ -6,8 +6,7 @@ dispatcher = updater.dispatcher
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
-picture ='https://www.crossed-flag-pins.com/animated-flag-gif/images/Flag_Russia.jpg'
-
+picture = 'https://www.crossed-flag-pins.com/animated-flag-gif/images/Flag_Russia.jpg'
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text='Привет, я - Country_by_flag_bot_436. Скоро будем отгадывать страны по флагу...надеюсь :)')
 def textMessage(bot, update):
@@ -18,7 +17,7 @@ def textMessage(bot, update):
         response = 'Напиши мне мб название своей страны на инглише ))0'
         bot.send_message(chat_id=update.message.chat_id, text=response)
 def photo(bot, update):
-    bot.sendPhoto(chat_id=update.message.chat_id, https://www.crossed-flag-pins.com/animated-flag-gif/images/Flag_Russia.jpg)
+    bot.sendPhoto(chat_id=update.message.chat_id, photo=picture)
 
 start_handler = CommandHandler('start', start)
 text_message_handler = MessageHandler(Filters.text, textMessage)
