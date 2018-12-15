32cc44ff70d60bd626c79e4718a4ffded64b236d
Maxim <therzmaxim@gmail.com>
2018-12-15 20:59:02 +0300

    random flags

--- bot.py
+++ bot.py
@@ -6,23 +6,20 @@ dispatcher = updater.dispatcher
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
-pic = 'http://flags.fmcdn.net/data/flags/w580/ru.png'
 index=39
 Data_countries = {'1':'ru', '2':'se','3':'us','4':'tr'}
-random_num=random.randint(1,4)
-number_for_dict=str(random_num)
-pic2=Data_countries.get(number_for_dict)
 
-def change_flag(pic):
+def change_flag():
+    pic = 'http://flags.fmcdn.net/data/flags/w580/ru.png'
     random_num = random.randint(1, 4)
     number_for_dict = str(random_num)
     pic2 = Data_countries.get(number_for_dict)
-    pic = pic[:index] + pic2+ pic[index + 2:]
+    pic = pic[:index] + pic2 + pic[index + 2:]
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text='Привет, я - Country_by_flag_bot_436. Скоро будем отгадывать страны по флагу...надеюсь :)')
 def textMessage(bot, update):
     if update.message.text != '':
-        change_flag(pic)
+        change_flag()
         response = 'Вот тебе флаг'
         bot.send_message(chat_id=update.message.chat_id, text=response)
         bot.sendPhoto(chat_id=update.message.chat_id, photo=pic)
