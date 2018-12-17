a5e5bb46ed38177fc631ca32a966578885aa813a
Maxim <therzmaxim@gmail.com>
2018-12-17 23:16:29 +0300

    last

--- bot.py
+++ bot.py
@@ -4,20 +4,19 @@ updater = Updater(token='700348901:AAGMYDZZmafglP6O9nAwn_yqpEfpzA8m_iw')
 dispatcher = updater.dispatcher
 
 import logging
-
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 index = 39
-Data_countries = {'1': 'ru', '2': 'se', '3': 'us', '4': 'tr', '5': 'dz', '6': 'ad', '7': 'ar', '8': 'am', '9': 'au',
-                  '10': 'at', '11': 'az', '12': 'by', '13': 'be', '14': 'br', '15': 'bg', '16': 'ca', '17': 'cl',
-                  '18': 'co', '19': 'hr', '20': 'cu', '21': 'cz', '22': 'dk', '23': 'eg', '24': 'ee', '25': 'fi',
-                  '26': 'fr', '27': 'ge', '28': 'de', '29': 'gr', '30': 'hu', '31': 'is', '32': 'in', '33': 'ir',
-                  '34': 'iq', '35': 'ie', '36': 'il', '37': 'it', '38': 'jm', '39': 'jp', '40': 'jo', '41': 'kz',
-                  '42': 'lv', '43': 'lt', '44': 'mx', '45': 'md', '46': 'nl', '47': 'nz', '48': 'kp', '49': 'no',
-                  '50': 'cn', '51': 'pl', '52': 'pt', '53': 'ro', '54': 'rs', '55': 'sk',
-                  '56': 'si', '57': 'kr', '58': 'es', '59': 'ch', '60': 'ua', '61': 'gb', '62': 'uy'}
-Data_name_countries = {'Россия': 'ru', 'Швеция': 'se', 'США': 'us', 'Турция': 'tr', 'Алжир': 'dz','Азербайджан':'ad', 'Аргентина': 'ar',
-                       'Армения': 'am', 'Австралия': 'au', 'Австрия': 'az','Беларусь':'by', 'Бельгия': 'be', 'Бразилия': 'br',
+Data_countries = {'1': 'ru', '2': 'se', '3': 'us', '4': 'tr', '5': 'dz', '6': 'az', '7': 'ar', '8': 'am', '9': 'au',
+                  '10': 'at', '11': 'by', '12': 'be', '13': 'br', '14': 'bg', '15': 'ca', '16': 'cl',
+                  '17': 'co', '18': 'hr', '19': 'cu', '20': 'cz', '21': 'dk', '22': 'eg', '23': 'ee', '24': 'fi',
+                  '25': 'fr', '26': 'ge', '27': 'de', '28': 'gr', '29': 'hu', '30': 'is', '31': 'in', '32': 'ir',
+                  '33': 'iq', '34': 'ie', '35': 'il', '36': 'it', '37': 'jm', '38': 'jp', '39': 'jo', '40': 'kz',
+                  '41': 'lv', '42': 'lt', '43': 'mx', '44': 'md', '45': 'nl', '46': 'nz', '47': 'kp', '48': 'no',
+                  '49': 'cn', '50': 'pl', '51': 'pt', '52': 'ro', '53': 'rs', '54': 'sk',
+                  '55': 'si', '56': 'kr', '57': 'es', '58': 'ch', '59': 'ua', '60': 'gb', '61': 'uy'}
+Data_name_countries = {'Россия': 'ru', 'Швеция': 'se', 'США': 'us', 'Турция': 'tr', 'Алжир': 'dz','Азербайджан':'az', 'Аргентина': 'ar',
+                       'Армения': 'am', 'Австралия': 'au', 'Австрия': 'at','Беларусь':'by', 'Бельгия': 'be', 'Бразилия': 'br',
                        'Болгария': 'bg', 'Канада': 'ca', 'Чили': 'cl', 'Колумбия': 'co', 'Хорватия': 'hr', 'Куба': 'cu',
                        'Чехия': 'cz', 'Дания': 'dk',
                        'Египет': 'eg', 'Эстония': 'ee', 'Финляндия': 'fi', 'Франция': 'fr', 'Грузия': 'ge',
@@ -43,22 +42,13 @@ def change_flag():
 
 
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id,
-                     text='Привет, я - Country_by_flag_bot_436. Скоро будем отгадывать страны по флагу...надеюсь :)')
     global active_game
     active_game = 1
-    bot.send_message(chat_id=update.message.chat_id,
-                     text='Привет, я - Country_by_flag_bot_436. Сейчас я пришлю тебе фото флага. Твоя задача - написать в ответ название этой страны на русском языке. Игра началась :)')
+    bot.send_message(chat_id=update.message.chat_id, text='Привет, я - Country_by_flag_bot_436. Сейчас я пришлю тебе фото флага. Твоя задача - написать в ответ название этой страны на русском языке. Игра началась :)')
     bot.sendPhoto(chat_id=update.message.chat_id, photo=pic)
 
 
 def textMessage(bot, update):
-    if update.message.text != '':
-        answer = update.message.text
-        change_flag()
-        response = 'Вот тебе флаг'
-        bot.send_message(chat_id=update.message.chat_id, text=response)
-        bot.sendPhoto(chat_id=update.message.chat_id, photo=pic)
     if active_game == 1:
         if update.message.text != '':
             answer = update.message.text
